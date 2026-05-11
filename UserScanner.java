c26f2cd8350c7dee342b2893f8f74b69691c7187
Durgamohansurnedd <durgamohan2003@gmail.com>
2026-05-11 20:51:35 +0530

    Scanner class in java

---
+++ UserScanner.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class UserScanner {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        int rollno=scanner.nextInt();
+        String name=scanner.next();
+        System.out.println ("rollno:"+rollno);
+        System.out.println("name:"+name);
+    }
+
+    
+}
