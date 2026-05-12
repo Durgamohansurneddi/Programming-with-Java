6b880788edff2ae9493f3d24302e3c3a8d6c8db3
Durgamohansurnedd <durgamohan2003@gmail.com>
2026-05-12 20:30:06 +0530

    Associativity example

---
+++ Associativity.java
@@ -0,0 +1,9 @@
+public class Associativity {
+    public static void main(String args[]){
+        int a=60/5-30*2 ;
+        // now the expression will use associativity of operators to solve the expression. like / and * operators have left to right associativity.
+        //  so first it will solve 60/5 then it will solve the remaining expression.
+        System.out.println(a);
+    }
+    
+}
---
+++ Precedence.java
@@ -0,0 +1,9 @@
+public class Precedence {
+    public static void main(String args[]){
+        int a=6*5-30/2; 
+        // now the expression will use precedence of operators to solve the expression. like *,/ has higher precedence than +,-.
+        //  so first it will solve 6*5 and 30/2 then it will solve the remaining expression.
+        System.out.println(a);
+    }
+    
+}
