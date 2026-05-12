1c83586efe5f8f5e51e062a0fcf11b39bd6ceef9
Durgamohansurnedd <durgamohan2003@gmail.com>
2026-05-12 21:05:17 +0530

    Increment and Decrement Operators in Java

---
+++ Incrementanddecrement.java
@@ -0,0 +1,23 @@
+public class Incrementanddecrement {
+    public static void main(String args[]){
+
+        //increment operator
+        int a=10;
+        //post increment
+        System.out.println(a++);//it will first print the value and later it will increment the value of a
+        System.out.println(a);//it will print the incremented value of a
+        //pre increment
+        System.out.println(++a);//it will first increment the value of a and later it will print the value of a
+        System.out.println(a);//it will print the incremented value of a}
+
+
+         //decrement operator
+         int b=5;
+        //post decrement
+        System.out.println(b--);//it will first print the value and later it will decrement the value of b
+        System.out.println(b);//it will print the decremented value of b
+        //pre decrement
+        System.out.println(--b);//it will first decrement the value of b and later it will print the value of b
+        System.out.println(b);//it will print the decremented value of b
+}
+}
