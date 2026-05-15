3691dfed1230db27e22461ffa5943040279b8d1b
Durgamohansurnedd <durgamohan2003@gmail.com>
2026-05-15 19:22:13 +0530

    While loop example in Java. This program prompts the user to enter a number and then prints all numbers from that number up to 100 using a while loop.

---
+++ Whileloop.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Whileloop {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("enter the number:");
+        int a=sc.nextInt();
+        while(a<=100){
+            System.out.println(a);
+            a++;
+        }
+        
+    }
+}
