33523f523811ae03af50af3f434d2c9e1bfed230
Durgamohansurnedd <durgamohan2003@gmail.com>
2026-05-15 20:10:37 +0530

    for loop and decrementing for loop examples

---
+++ Forloop.java
@@ -0,0 +1,17 @@
+public class Forloop {
+    public static void main(String args[]){
+        int a=10;
+        for(int i=0;i<10;i++){
+            System.out.println(2*i+1);
+        }
+        for(int j=1;j<10;j++){
+            System.out.println(2*j);
+        }
+
+        //Decerement for loop
+        for(int k=50;k>=0;k--){
+            System.out.println(k);
+        }
+    }
+    }
+
