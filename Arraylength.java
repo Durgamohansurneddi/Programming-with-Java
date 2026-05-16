d1895af64a8483b38fa58e5b76a76cf29f5e1a1b
Durgamohansurnedd <durgamohan2003@gmail.com>
2026-05-16 18:19:50 +0530

    Length of array in java

---
+++ Arraylength.java
@@ -0,0 +1,7 @@
+public class Arraylength {
+    public static void main(String[] args) {
+        int[] arr={10,20,30,40,50};
+        int length=arr.length;
+        System.out.print("length of array:"+length);
+    }
+}
