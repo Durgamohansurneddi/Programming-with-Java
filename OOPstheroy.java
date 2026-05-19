c290a4b33e776eecaf461e7473c6e184f4085fb5
Durgamohansurnedd <durgamohan2003@gmail.com>
2026-05-19 18:47:43 +0530

    OOPs concept in theroy in java

---
+++ OOPstheroy.java
@@ -0,0 +1,24 @@
+public class OOPstheroy {
+//Object-Oriented Programming (OOP) is a way of writing programs where we think in terms of objects.
+//OOPs has four pillars
+/*  Encapsulation:Think of it as hiding details.
+Example: A TV remote lets you change channels without knowing how the circuits inside work.
+In Java, we use private variables and public methods to control access.
+
+
+Abstraction
+Focus only on important details, ignore the rest.
+Example: When you drive a car, you just use the steering wheel and pedals — you don’t worry about the engine’s inner parts.
+In Java, we use abstract classes and interfaces.
+
+Inheritance
+One class can get features from another class.
+Example: A “Dog” class can inherit from an “Animal” class, so it automatically has things like eat() or sleep().
+In Java, we use the extends keyword.
+
+Polymorphism
+Means many forms — the same action can behave differently.
+Example: A sound() method makes a Dog bark and a Cat meow.
+In Java, this happens through method overloading (compile-time) and method overriding (runtime).*/
+
+}
